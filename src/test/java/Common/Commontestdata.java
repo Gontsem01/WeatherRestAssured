656e894ac928e105343cc008c7fa3f80a18195bc
Admin <gontse.mmoke@gmail.com>
2025-10-27 14:00:21 +0200

    added baseURL and APIkey classes under Commontestdata

---
+++ src/test/java/Common/Commontestdata.java
@@ -0,0 +1,8 @@
+package Common;
+
+public class Commontestdata {
+
+    public static String weatherBaseUrl = "https://api.openweathermap.org/data/3.0/stations";
+    public static String weatherApiKey = "9223f256b9f8e26f8419afcc9389d7ef";
+
+}
